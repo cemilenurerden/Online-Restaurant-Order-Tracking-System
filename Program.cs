4ff3a8356fb273dca94b50c299e6aae9735a61c9
esmanurgokkaya <esmanurgkky10@gmail.com>
2024-11-25 23:25:09 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Online_Restaurant_Order_Tracking_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles(); 
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
